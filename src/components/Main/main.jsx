64cfb97f5dcae0d72a946a84d1f66a6c0522f440
julio@sparta66 <julioh.vivancos@gmail.com>
2025-05-20 16:07:45 -0300

    V3 - Arrumando o main

--- src/components/Main/main.jsx
+++ src/components/Main/main.jsx
@@ -72,11 +72,14 @@ const Main = () => {
             <button className="main-button button-1" onClick={openModal}>
               Gerar Playlist
               <p className="main-button-text">
-                Clique para gerar uma playlist de acordo com seu humor e o gênero
-                musical que você escolher
+                Clique para gerar uma playlist de acordo com seu humor e o
+                gênero musical que você escolher
               </p>
             </button>
-            <button className="main-button button-2" onClick={openPlaylistModal}>
+            <button
+              className="main-button button-2"
+              onClick={openPlaylistModal}
+            >
               <h1 className="main-button-title">Playlists Geradas</h1>
               <p className="main-button-text">
                 Visualize as suas playlists geradas
