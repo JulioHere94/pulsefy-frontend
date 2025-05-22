d04291ed31fa37983b563dd8503f65f3df1d7cd9
julio@sparta66 <julioh.vivancos@gmail.com>
2025-05-22 19:08:32 -0300

    V4 - Arrumando problemas na ligação entre back e frontend

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,10 +2,11 @@ import axios from "axios";
 import authService from "../services/authService";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
   headers: {
     "Content-Type": "application/json",
   },
+  withCredentials: true,
 });
 
 // Request interceptor
